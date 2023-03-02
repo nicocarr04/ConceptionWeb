88f9da068fcedc081c16ba01590efb0ae7630828
nicocarr04 <77184162+nicocarr04@users.noreply.github.com>
2023-03-02 17:02:06 -0500

    Ajout de professeurid dans models Matieres
    Signed-off-by: nicocarr04 <77184162+nicocarr04@users.noreply.github.com>

--- models/Matieres.js
+++ models/Matieres.js
@@ -1,6 +1,7 @@
 import { DataTypes } from "sequelize"
 
 const matieres = database.define('matieres', {
+    professeurid: { type: DataTypes.INTEGER, allowNull: false },
     titre: { type: DataTypes.STRING, allowNull: false }
 },
 { timestamps: false} 
